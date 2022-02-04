8708b4e69ad1124f529d262e5c39355c9f8eb13a
Pietrodon <donvitopietro00@gmail.com>
2022-02-04 16:45:53 +0100

    inserimento prompt per numero di km ed eta e controllo che l'utente inserisca valori numerici

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 
-const numKm = prompt('quanti km vuoi percorrere?')
-console.log(numKm)
+const numKm =  parseInt ( prompt ('quanti km vuoi percorrere?') );
+console.log(numKm);
+
+if  ( isNaN (numKm) ) {
+    alert('il dato km deve avere un valore numerico');
+} else {
+    const userAge = parseInt ( prompt ('quanti anni hai??') );
+    console.log(userAge);
+ // console.log(typeof userAge);
+        if ( isNaN (userAge) ) {
+            alert('il dato età deve avere un valore numerico');
+        }
+}
+
+const ticketPrice = numKm * 0.21 ;
+console.log(ticketPrice)
+
+if (userAge<18){
+    
+}
+
+
+
+
+
 
 
