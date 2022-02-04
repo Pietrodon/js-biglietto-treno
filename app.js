02afc4256eda30eb75970ca1f6e1de270e28e035
Pietrodon <donvitopietro00@gmail.com>
2022-02-04 17:24:26 +0100

    creazione if per calcolo prezzo del biglietto in base all'eta inserita

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-
-const numKm =  parseInt ( prompt ('quanti km vuoi percorrere?') );
+let numKm =  parseInt(prompt('quanti km vuoi percorrere?'));
 console.log(numKm);
+// let userAge = 0
 
-if  ( isNaN (numKm) ) {
+if (isNaN(numKm)) {
     alert('il dato km deve avere un valore numerico');
+    numKm = parseInt(prompt('quanti km vuoi percorrere?'));
 } else {
-    const userAge = parseInt ( prompt ('quanti anni hai??') );
-    console.log(userAge);
- // console.log(typeof userAge);
-        if ( isNaN (userAge) ) {
-            alert('il dato età deve avere un valore numerico');
-        }
+
+
 }
+
+
+let userAge = parseInt(prompt('quanti anni hai??'));
+console.log(userAge)
+if (isNaN(userAge)){
+    alert('il dato età deve avere un valore numerico');
+    userAge = parseInt(prompt('quanti anni hai??'));
+}
+
 
 const ticketPrice = numKm * 0.21 ;
 console.log(ticketPrice)
 
-if (userAge<18){
+if ( userAge < 18 ){
+    let discount = ((ticketPrice * 20) / 100);
+    let newTicketPrice = (ticketPrice - discount);
+    console.log(newTicketPrice)
+} else if ( userAge > 65){
+    let discount = ((ticketPrice * 40) / 100);
+    let newTicketPrice = (ticketPrice - discount);
+    console.log(newTicketPrice)
+} else{
     
 }
 
